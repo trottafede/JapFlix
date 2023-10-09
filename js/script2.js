525dccbc20354fb01ac0b58c3e8aee18ceb740f2
Federico Trotta <ftrotta18@gmail.com>
2023-10-08 21:30:05 -0300

    Todo list

--- js/script2.js
+++ js/script2.js
@@ -1,90 +1,106 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const inputBuscar = document.getElementById("inputBuscar");
-    const buttonBuscar = document.getElementById("btnBuscar");
-    const listaPeliculas = document.getElementById("lista");
-    
-    const contenedorInfo = document.createElement("div");
-    contenedorInfo.classList.add("container", "mt-3", "border", "p-3", "bg-light", "ayuda");
-    contenedorInfo.style.display = "none"; // Oculta el contenedor al principio
+  const inputBuscar = document.getElementById("inputBuscar");
+  const buttonBuscar = document.getElementById("btnBuscar");
+  const listaPeliculas = document.getElementById("lista");
+  let cantidadPeliculasEncontradas = 0;
 
-fetch('https://japceibal.github.io/japflix_api/movies-data.json')
-  .then(response => response.json())
-  .then(data => {
-        const peliculasData = data;
+  const contenedorInfo = document.createElement("div");
+  contenedorInfo.classList.add(
+    "container",
+    "mt-3",
+    "border",
+    "p-3",
+    "ayuda"
+  );
+  contenedorInfo.style.display = "none"; // Oculta el contenedor al principio
 
-        function mostrarPeliculas(query) {
-          listaPeliculas.innerHTML = '';
-      
-          const peliculasFiltradas = peliculasData.filter(peliculas => {
-              const { title, genres, tagline, overview } = peliculas;
-              const buscarCaracteristica = query.toLowerCase();
-              return (
-                 title.toLowerCase().includes ( buscarCaracteristica) ||
-                  genres[0].name.toLowerCase().includes(buscarCaracteristica) ||
-                  tagline.toLowerCase().includes(buscarCaracteristica) ||
-                  overview.toLowerCase().includes(buscarCaracteristica)
-              );
+  fetch("https://japceibal.github.io/japflix_api/movies-data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const peliculasData = data;
+      function mostrarPeliculas(query) {
+        listaPeliculas.innerHTML = "";
+        const peliculasFiltradas = peliculasData.filter((peliculas) => {
+          const { title, genres, tagline, overview } = peliculas;
+          const buscarCaracteristica = query;
+          return (
+            title.includes(buscarCaracteristica)
+            || genres.find((genero) => genero.name.toLowerCase() === buscarCaracteristica.toLowerCase())
+            || tagline.toLowerCase().includes(buscarCaracteristica)
+            || overview.toLowerCase().includes(buscarCaracteristica)
+          );
+        }
+        );
+
+        peliculasFiltradas.forEach((peliculas) => {
+          const divPeliculas = document.createElement("div");
+          divPeliculas.classList.add("peliculas");
+          divPeliculas.addEventListener("click", () => {
+            funcionDeChiara(peliculas);
           });
+          divPeliculas.innerHTML = `
+            <h2>${peliculas.title}</h2>
+            <p>${peliculas.tagline}</p>
+            <p>Calificacion:<span class="estrellas-color"> ${peliculas.vote_average} - ${obtenerEstrellas(peliculas.vote_average)}</span></p>
+            <hr>`;
+          listaPeliculas.appendChild(divPeliculas);
+        });
+
+
+        cantidadPeliculasEncontradas = peliculasFiltradas.length;
+        document.getElementById("numberOfQuerys").innerHTML = "Cantidad de resultados: " + cantidadPeliculasEncontradas;
+      }
+      function yearDate(date){
+        const year = date.release_date.split('-')
+        return year[0]
+      }
+        
+      function funcionDeChiara(pelicula) {
+        let generos = "";
+        pelicula.genres.map((genero, index) => {
+          generos = generos + genero.name + " ";
+        });
+        contenedorInfo.innerHTML = ` 
+              <h2>${pelicula.title}</h2>
+                <p>${pelicula.overview}</p>
+                <p><strong>Géneros:</strong> ${generos}</p>
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Mas información
+                </button>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Year : ${yearDate(pelicula)}</a></li>
+                    <li><a class="dropdown-item" href="#">Runtime : ${pelicula.runtime} mins</a></li>
+                    <li><a class="dropdown-item" href="#">Budget : $${pelicula.budget}</a></li>
+                    <li><a class="dropdown-item" href="#">Revenue : $${pelicula.revenue}</a></li>
+                  </ul>
+                </div>
+              `;
+
+        contenedorInfo.style.display = "block";
+        const hola = document.getElementById("hola")
+        const main = document.getElementById("MyTitle");
+        main.insertBefore(contenedorInfo, hola);
+        }
+
+      function obtenerEstrellas(rating) {
+        const newRating = rating / 2;
+        const estrellasColoreadas = Math.floor(newRating); // estrellas doradas
+        const  estrellasSinColorear = 5 - estrellasColoreadas;
+        const calificado = `<i class="fa-regular fa-star"></i>`;
+        const sinCalificar = `<i class="fa-solid fa-star"></i>`;
+        
+        let resultado = ""
+        resultado += sinCalificar.repeat(estrellasColoreadas) + calificado.repeat(estrellasSinColorear);
       
-          peliculasFiltradas.forEach(peliculas => {
-              const divPeliculas = document.createElement('div');
-              divPeliculas.classList.add('peliculas');
-               divPeliculas.addEventListener("click", () => {
-                funcionDeChiara(peliculas);
-              }); 
-              divPeliculas.innerHTML = `
-                  <h2>${peliculas.title}</h2>
-                  <p>${peliculas.tagline}</p>
-                  <p>Calificacion:<span class="estrellas-color"> ${obtenerEstrellas(peliculas.vote_average)}</span></p>
-                  <hr>`;
-              listaPeliculas.appendChild(divPeliculas);
-          });
-}
+        return resultado;
+      }
 
-function funcionDeChiara(pelicula) {
-  let generos = "";
-  pelicula.genres.map((genero,index) => {
-    generos = generos + genero.name + " " ;
-  } )
-  contenedorInfo.innerHTML = ` 
-  <h2>${pelicula.title}</h2>
-    <p>${pelicula.overview}</p>
-    <p><strong>Géneros:</strong> ${generos}</p>
-    <div class="dropdown">
-      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-       Mas información
-     </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Year : ${pelicula.release_date}</a></li>
-        <li><a class="dropdown-item" href="#">Runtime : ${pelicula.runtime}</a></li>
-        <li><a class="dropdown-item" href="#">Budget : ${pelicula.budget}</a></li>
-        <li><a class="dropdown-item" href="#">Revenue : ${pelicula.revenue}</a></li>
-      </ul>
-    </div>
-  `;
-
-  contenedorInfo.style.display = "block";
-  document.body.insertBefore(contenedorInfo, document.getElementById("MyTitle"));
-}
-
-function obtenerEstrellas(rating){
-    const newRating = rating/2;
-    const redondear = Math.round(newRating );
-    const stars = '★'.repeat(redondear);
-    return stars
-}
-
-buttonBuscar.addEventListener('click', () => {
-    const buscarTermino = inputBuscar.value.trim();
-    if(buscarTermino){
-        mostrarPeliculas(buscarTermino);
-    }
+      buttonBuscar.addEventListener("click", () => {
+        const buscarTermino = inputBuscar.value.trim();
+        if (buscarTermino) {
+          mostrarPeliculas(buscarTermino);
+        }
+      });
+    });
 });
-})
-
-
-
-});
-
-      
-          
--- readme.md
+++ readme.md
@@ -4,9 +4,7 @@ Modificar el sitio web adjunto para lograr cumplir con las siguientes pautas:
 1) Cuando la página cargue, deberá traer el listado de información sobre películas disponible en 
 https://japceibal.github.io/japflix_api/movies-data.json, pero no mostrarlo al usuario.
 
-
 2) Cuando el usuario presiona el botón buscar, y si previamente ingresó algún valor en el campo de búsqueda, deberá mostrar un listado con las películas que coincidan con dicha búsqueda en sus atributos de title o genres o tagline u overview. La información a mostrar en este punto será: title, tagline, y vote_average (en formato de "estrellas").
-
 
 3) Cuando el usuario pulse en alguna de las películas mostradas, se deberá desplegar un contenedor superior con la siguiente información de dicha película: title, overview y lista de genres.
 
@@ -17,3 +15,32 @@ https://japceibal.github.io/japflix_api/movies-data.json, pero no mostrarlo al u
 ATENCIÓN!!!:
 
 Las imágenes proporcionadas en todos los casos son meramente ilustrativas, y están presentes para brindar entendimiento visual sobre cada pauta. No se espera que el resultado cumpla con los mismos criterios de estilo específicos. 
+
+//to do
+2) 
+  a) Arreglar filtro
+
+3)
+  a) Adaptarlo con un top offcanvas de bootstrap.
+
+4)
+  a) quitarle mes y dia x
+  b) a la duracion agregarle "mins" x
+  c) a las ganancias y presupuesto agregarle signo de pesos x
+
+5) Responsive?
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {...}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {...}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {...}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {...}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {...}
